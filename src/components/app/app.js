e35fc6576d9b71f1bb32a2df3c2692901062314a
akoross <74363342+akoross@users.noreply.github.com>
2021-02-09 23:12:52 +0200

    v2-1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import ErrorIndicator from "../error-indicator/error-indicator";
 import Header from "../header/header";
 import ItemDetails from "../item-details/item-details";
 import ItemList from "../item-list/item-list";
 import RandomPLanet from "../random-planet/random-planet";
 
 export default class App extends Component {
-   state = { id: null };
+   state = { id: null, hasError: false };
 
    onItemSelected = (id) => {
       this.setState({
@@ -13,9 +14,15 @@ export default class App extends Component {
       });
    };
 
-   render() {
-      const { id } = this.state;
+   componentDidCatch() {
+      this.setState({ hasError: true });
+   }
 
+   render() {
+      const { id, hasError } = this.state;
+      if (hasError) {
+         return <ErrorIndicator />;
+      }
       return (
          <div className="app container">
             <Header className="row" />
